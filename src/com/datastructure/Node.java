7cb10f9e033946e3c28b3b2172c5a8efdb0d9fda
ASHU-code06 <ashuc306@gmail.com>
2023-03-10 03:25:16 +0530

    Made a linked list , and give input from main method

--- src/com/datastructure/Node.java
+++ src/com/datastructure/Node.java
@@ -14,22 +14,14 @@ public class Node {
         this.data = data;
         this.next = null;
     }
-    public void creation(){
-        int d,n ;
-        do{
-            System.out.println("Enter data");
-            Scanner input = new Scanner(System.in);
-            d = input.nextInt();
-            Node newNode = new Node(d);
-            if(head==null){
-                head = newNode;//if linked list is  empty then create first node to the linked list
-            }else{
-                newNode.next=head;//the address inside head is assigned to next of newNode
-                head=newNode;//data inside newNode is assigned to head
-            }
-            System.out.println("Do you want to enter more data than press 1 else press 2 to exit");
-            n = new Scanner(System.in).nextInt();
-        }while (n==1);
+    public void creation(int d){
+        Node newNode = new Node(d);
+        if(head==null){
+            head = newNode;//if linked list is  empty then create first node to the linked list
+        }else{
+            newNode.next=head;//the address inside head is assigned to next of newNode
+            head=newNode;//data inside newNode is assigned to head
+        }
     }
     public void traverse(){
         Node tempNode = head;//declaration and initialization a tempNode of type Node
