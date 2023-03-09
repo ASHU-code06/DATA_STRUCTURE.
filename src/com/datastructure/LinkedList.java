99c62c0ecd8c765af670c2b6068fe434eecf2c5f
ASHU-code06 <ashuc306@gmail.com>
2023-03-10 04:53:53 +0530

    Made a linked list , and added 'addAtDesiredPos'  method to add a node at any desired position in likned list

--- src/com/datastructure/LinkedList.java
+++ src/com/datastructure/LinkedList.java
@@ -4,8 +4,9 @@ public class LinkedList {
     public static void main(String[] args) {
     Node node = new Node();
     node.creation(56);
-    node.creation(30);
-    node.creation(70);
+    node.append(70);
+    node.traverse();
+    node.addAtDesiredPos(30);
     node.traverse();
     }
 }
