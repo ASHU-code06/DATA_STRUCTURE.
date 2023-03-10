08cf2c1e6fbe71c20fe5bb9df31c5d88b6afd58e
ASHU-code06 <ashuc306@gmail.com>
2023-03-10 17:21:50 +0530

    Made a linked list , and added 'search()'  method to search a node at last position in likned list

--- src/com/datastructure/LinkedList.java
+++ src/com/datastructure/LinkedList.java
@@ -6,8 +6,12 @@ public class LinkedList {
     node.creation(56);
     node.append(70);
     node.traverse();
-    node.addAtDesiredPos(30);
+    node.addAtDesiredPos(30,2);
     node.traverse();
     node.deleteLast();
+    boolean checkNode = node.search(39);
+    if(!checkNode){
+        System.out.println("Linked list does not have the node");
+    }
     }
 }
