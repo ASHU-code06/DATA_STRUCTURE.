e247c835e8b1b74d3b02ba781d78d7734a46a556
ASHU-code06 <ashuc306@gmail.com>
2023-03-11 00:28:13 +0530

    added append method , display method of queue

---
+++ src/com/datastructure/QueueUsingLinkedList.java
@@ -0,0 +1,29 @@
+package com.datastructure;
+
+public class QueueUsingLinkedList extends Node {
+    Node front = null;
+    Node rear=null;
+    void enqueue(Object data) {
+        append(data);
+    }
+    void display() {
+        Node temp = front;//pointer of type Node
+        if (front == null) {
+            System.out.println("This queue is empty");}
+        while (temp != null) {
+            System.out.print(temp.data+"->");
+            temp = temp.next;
+        }
+    }
+    void append(Object data) {
+        Node new_node = new Node(data);
+        if (front == null) {
+            front = new_node;
+            rear = new_node;
+        } else {
+            rear.next=new_node;
+            rear=new_node;
+        }
+    }
+
+}
