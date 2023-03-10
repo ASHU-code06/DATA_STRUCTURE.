353c717a54b5670c9f385f2772b2d788a41fd9bc
ASHU-code06 <ashuc306@gmail.com>
2023-03-10 20:59:56 +0530

    made push() , pop() , display() method and used inheritance to create a stack with value 56 on top

---
+++ src/com/datastructure/Stack.java
@@ -0,0 +1,11 @@
+package com.datastructure;
+
+public class Stack {
+    public static void main(String[] args) {
+        StackUsingLinkedList object = new StackUsingLinkedList();
+        object.push(70);
+        object.push(30);
+        object.push(56);
+        object.display();
+    }
+}
